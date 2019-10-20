599c2b2a5e427e91d3c7fa52f92f7937713a58d6
sanzor <adriansan_93@yahoo.com>
2019-10-20 15:55:56 +0300

    finished server

--- myserver.erl
+++ myserver.erl
@@ -1,17 +1,63 @@
 -module(myserver).
+
 -record(cat,{name,color=green,description}).
--export([start_link/0,order_cat/4,return_cat/4,close_shop/1]).
 
 start_link()->spawn_link(fun init/0).
+
+init()->
+    loop([]).
 
 order_cat(Pid,Name,Color,Description)->
     Ref=erlang:monitor(process,Pid),
     Pid ! {self(),Ref,{order,Name,Color,Description}},
-    receive
-        {Ref,Cat}->erlang:demonitor(process,[flush]),
-                   Cat;
-        {'DOWN',MsgRef,process,Pid,Reason}->
-            erlang:error(Reason)
-    after 5000
+    receive 
+        {Ref,Cat}->
+            erlang:demonitor(Ref,[flush]),
+            Cat;
+        {'DOWN',Ref,process,Pid,Reason}->
+            Pid ! error(Reason)
+    after 5000 ->
         erlang:error(timeout)
     end.
+
+
+close_shop(Pid)->
+    Ref=erlang:monitor(process,Pid),
+    Pid ! {self(),Ref,terminate},
+    receive 
+        {Ref,ok}->erlang:demonitor(Ref,[flush]),
+                  ok;
+        {'DOWN',Ref,process,Pid,Reason}->
+            erlang:error(Reason)
+    after 5000 ->
+        erlang:error(timeout)
+    end.
+
+return_cat(Pid,Cat=#cat{})->
+    Pid ! {return,Cat},
+    ok.
+
+loop(Cats)->
+    receive 
+    {Pid,Ref,{order,Name,Color,Description}}->
+        if Cats =:= [] ->
+              Pid !{Ref,make_cat(Name,Color,Description)},
+              loop(Cats);
+           Cats =/=[] ->
+              Pid ! {Ref,hd(Cats)},
+              loop(tl(Cats))
+        end;
+    {Pid,Ref,terminate}->
+                Pid !{Ref,ok},
+                terminate(Cats);
+    {return,Cat=#cat{}}->
+        loop([Cat|Cats]);
+    Unknown -> io:format("unnkown message"),
+               loop(Cats)
+    end.
+make_cat(Name,Color,Description)->#cat{name=Name,color=Color,description=Description}.
+
+terminate(Cats)->
+    [io:format("~p was set free",C#cat.name)|| C <-Cats ].
+    
+
